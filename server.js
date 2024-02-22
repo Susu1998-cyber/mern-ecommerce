085bcae9e0d893ab266c549fc47341442a6c5f42
Susu1998-cyber <130694657+Susu1998-cyber@users.noreply.github.com>
2024-02-22 21:17:42 +0530

    Updated

--- server.js
+++ server.js
@@ -4,11 +4,13 @@
  import morgan from 'morgan';
 import connectDB from './config/db.js';
 import authRoutes from './routes/authRoute.js';
-import categoryRoutes from './routes/CategoryRoutes.js'
+ 
 import productRoutes from './routes/productRoutes.js'
  import cors from 'cors'
  import path from 'path'
 import {fileURLToPath} from 'url'
+import categoryRoutes from './CategoryRoutes.js';
+
 
 
  
