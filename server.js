90d76d68e6be5faa38eebbac0ad942d61a30f7a0
Susu1998-cyber <130694657+Susu1998-cyber@users.noreply.github.com>
2024-02-22 20:49:08 +0530

    Updated

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ import categoryRoutes from './routes/CategoryRoutes.js'
 import productRoutes from './routes/productRoutes.js'
  import cors from 'cors'
  import path from 'path'
+import {fileURLToPath} from 'url'
+
 
  
 
@@ -16,7 +18,9 @@ import productRoutes from './routes/productRoutes.js'
 
  //database config
  connectDB();
-
+//esmodule fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 // rest object
 const app = express()
 
